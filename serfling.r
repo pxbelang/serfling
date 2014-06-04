c42310fdb3bb5d2c968f0c89393edcd560f422c3
pxbelang <pxbelang@gmail.com>
2014-06-04 11:06:02 -0400

    changes necessary to estimate syndromes 2 and 3 separately (i.e., generate separate Serfling curves)

--- serfling.r
+++ serfling.r
@@ -1,5 +1,6 @@
+serfling <- read.csv('serflingSyn23.csv')
 fluseason <- subset(serfling, WofY > 18 & WofY < 40)
-serfreg <- lm(pctRespFeverILI ~ Week + I(Week*Week) + cos(2*pi*Week/52) + sin(2*pi*Week/52), data=fluseason)
+serfreg <- lm(pctSyn3 ~ Week + I(Week*Week) + cos(2*pi*Week/52) + sin(2*pi*Week/52), data=fluseason)
 serfconf  <- predict(serfreg, newdata = serfling, interval="confidence", level=0.95)
 serffinal <- cbind(serfling, serfconf)
 
@@ -11,4 +12,4 @@ lines(serffinal$upr, type='l',col="red")
 x <- data.frame(c(1:600))
 colnames(x) <- c("Week")
 serftmp <- predict(serfreg, newdata = x, interval="confidence", level=0.95)
-write.csv(serftmp, file="c:\\temp\\serfling.csv")
+write.csv(serftmp, file="c:\\temp\\serflingSyn2.csv")
